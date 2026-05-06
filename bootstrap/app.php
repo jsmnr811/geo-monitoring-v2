9a19e800bc2efb43fa54303e109b321f59896317
jsmnr811 <jasmin.aure.4@gmail.com>
2026-05-06 11:00:27 +0800

    update schedule to every 3 AM

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,9 +7,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
+        channels: __DIR__ . '/../routes/channels.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -18,7 +18,15 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->withSchedule(function (Schedule $schedule) {
-        $schedule->command('sidlan:sync')->everyMinute()->withoutOverlapping();
-        $schedule->command('sidlan:sync-progress')->everyMinute()->withoutOverlapping();
-        $schedule->command('gms:sync-albums')->everyMinute()->withoutOverlapping();
+        $schedule->command('sidlan:sync')
+            ->dailyAt('03:00')
+            ->withoutOverlapping();
+
+        $schedule->command('sidlan:sync-progress')
+            ->dailyAt('03:00')
+            ->withoutOverlapping();
+
+        $schedule->command('gms:sync-albums')
+            ->dailyAt('03:00')
+            ->withoutOverlapping();
     })->create();
