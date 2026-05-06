9b1347cd8aa8bb62dbcee2b8598146ee5d33c9cd
jsmnr811 <jasmin.aure.4@gmail.com>
2026-05-06 08:37:37 +0800

    Remove progress_only in user tb

---
+++ database/migrations/2026_05_06_003414_drop_gms_progress_only_from_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (Schema::hasColumn('users', 'gms_progress_only')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('gms_progress_only');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('gms_progress_only')->nullable();
+        });
+    }
+};
